cabc6244b9f7229948fad21f37813e2a090d6814
TomaszN <bomek1@gmail.com>
2012-10-16 13:27:33 +0200

    numerowanie od 1

--- Form1.cs
+++ Form1.cs
@@ -46,34 +46,34 @@ namespace nehisa
             test = sr2.ReadLine();
             this.proces1.n = Convert.ToInt32(test);
 
-            this.proces1.NT = new Int32[this.proces1.n];
-            this.proces1.NM = new Int32[this.proces1.n];
-            this.proces1.P = new Int32[this.proces1.n];
+            this.proces1.NT = new Int32[this.proces1.n+1];
+            this.proces1.NM = new Int32[this.proces1.n+1];
+            this.proces1.P = new Int32[this.proces1.n+1];
             
 
 
             test = sr2.ReadLine();
             string[] exploded2 = test.Split(' ');
             
-            for (i = 0; i < this.proces1.n; i++)
+            for (i = 1; i <= this.proces1.n; i++)
             {
-                this.proces1.NT[i] = Convert.ToInt32(exploded2[i]);
+                this.proces1.NT[i] = Convert.ToInt32(exploded2[i-1]);
             }
 
             test = sr2.ReadLine();
             string[] exploded3 = test.Split(' ');
 
-            for (i = 0; i < this.proces1.n; i++)
+            for (i = 1; i <= this.proces1.n; i++)
             {
-                this.proces1.NM[i] = Convert.ToInt32(exploded3[i]);
+                this.proces1.NM[i] = Convert.ToInt32(exploded3[i-1]);
             }
 
             test = sr2.ReadLine();
             string[] exploded4 = test.Split(' ');
 
-            for (i = 0; i < this.proces1.n; i++)
+            for (i = 1; i <= this.proces1.n; i++)
             {
-                this.proces1.NM[i] = Convert.ToInt32(exploded4[i]);
+                this.proces1.P[i] = Convert.ToInt32(exploded4[i-1]);
             }
 
             test = sr2.ReadLine();
@@ -113,10 +113,95 @@ namespace nehisa
         private void button4_Click(object sender, EventArgs e)
         {
             int i;
+            int a, b, c;
+
+            this.proces1.PT = new Int32[this.proces1.n+1];
+            this.proces1.PM = new Int32[this.proces1.n+1];
+            this.proces1.LP = new Int32[this.proces1.n+1];
+
+            for (i = 1; i <= this.proces1.n; i++)
+            {
+                this.proces1.PT[i] = 0;
+                this.proces1.PM[i] = 0;
+                this.proces1.LP[i]=0;
+            }
+
+            for(i=1; i<=this.proces1.n; i++)
+            {
+                a = this.proces1.NT[i];
+                if (a > 0)
+                {
+                    this.proces1.PT[a] = i;
+                }
+
+                b = this.proces1.NM[i];
+                if (b > 0)
+                {
+                    this.proces1.PM[b] = i;
+                }
+
+            }
+
+            for (i = 1; i <= this.proces1.n; i++)
+            {
+                if(this.proces1.PT[i]>0)
+                {
+                    this.proces1.LP[i]++;
+                }
+
+                if (this.proces1.PM[i] > 0)
+                {
+                    this.proces1.LP[i]++;
+                }
+            }
+
+            this.proces1.kolejnosc = new List<int>();
+
+            for (i = 1; i <= this.proces1.n; i++)   //pierwsze przegladniecie
+            {
+                if (this.proces1.LP[i] == 0)
+                {
+                    this.proces1.kolejnosc.Add(i);
+                }
+            }
+
+            i = 0;
+
+            while (this.proces1.kolejnosc.Count < this.proces1.n)   //nie wiem czy może być taki warunek - w momencie jak nie będzie dało się ustalić koeljności się wysypie
+            {
+                a = this.proces1.kolejnosc[i];
+                b=this.proces1.NT[a];
+                c=this.proces1.NM[a];
+
+                this.proces1.LP[b]--;
+                if (this.proces1.LP[b] == 0)
+                {
+                    this.proces1.kolejnosc.Add(b);
+                }
+
+                this.proces1.LP[c]--;
+                if (this.proces1.LP[c] == 0)
+                {
+                    this.proces1.kolejnosc.Add(c);
+                }
+
+                i++;
+
+            }
+
+            b = 123;
+
 
             this.proces1.C = new Int32[this.proces1.n];
             this.proces1.S = new Int32[this.proces1.n];
 
+            for (i = 1; i < this.proces1.n; i++ )
+            {
+                if(this.proces1.LP[i]==0)
+                {
+                    
+                }
+            }
             for (i = 0; i < this.proces1.n; i++)
             {
                 this.proces1.C[i] = i;
@@ -132,8 +217,13 @@ namespace nehisa
         public int m = 0;
         public int[] NT;
         public int[] NM;
+        public int[] PT;
+        public int[] PM;
+        public int[] LP;
         public int[] P;
         public int[] permutacja;
+
+        public List<Int32> kolejnosc;
 
         public int[] C;
         public int[] S;
